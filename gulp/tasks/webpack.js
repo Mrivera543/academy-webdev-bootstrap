07c66058ae036541a8529b8206fb5a775abeb6db
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2015-06-15 02:22:13 +0200

    Added task for webpack

---
+++ gulp/tasks/webpack.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+var webpack = require('gulp-webpack')
+var config = require('../config')
+
+gulp.task('webpack', function () {
+  return gulp.src(config.js.src)
+    .pipe(webpack(config.webpack))
+    .pipe(gulp.dest(config.js.dest))
+})
